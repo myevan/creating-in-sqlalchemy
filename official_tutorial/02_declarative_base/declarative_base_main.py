359c90c95d2d8661918725cfddb185a02887509a
jaru <jaru@moonrabbit.co>
2015-01-06 15:44:18 +0900

    ! User 클래스 외부로 이동 @ official_tutorial/02_declarative_base

--- official_tutorial/02_declarative_base/declarative_base_main.py
+++ official_tutorial/02_declarative_base/declarative_base_main.py
@@ -4,16 +4,17 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return '<User(id={0}, name={1})>'.format(self.id, self.name)
+
+
 if __name__ == '__main__':
-    class User(Base):
-        __tablename__ = 'users'
-
-        id = Column(Integer, primary_key=True)
-        name = Column(String)
-
-        def __repr__(self):
-            return '<User(id={0}, name={1})>'.format(self.id, self.name)
-
     user = User(id=0, name='guest')
     print(user)
 
