af3ca868bd72e71451112edd0bceca0a4ac6550a
jaru <jaru@moonrabbit.co>
2015-01-06 15:39:59 +0900

    + 03_declarative_base @ official_tutorial

---
+++ official_tutorial/03_declarative_base/declarative_base_main.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+if __name__ == '__main__':
+    class User(Base):
+        __tablename__ = 'users'
+
+        id = Column(Integer, primary_key=True)
+        name = Column(String)
+
+        def __repr__(self):
+            return '<User(id={0}, name={1})>'.format(self.id, self.name)
+
+    user = User(id=0, name='guest')
+    print(user)
+
