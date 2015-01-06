d6a15598f9cf6277f67b2193b7ea6688cb14c508
jaru <jaru@moonrabbit.co>
2015-01-06 15:48:05 +0900

    ! 메인 파일 이름 변경 @ official_tutorial/03_db_engine

--- official_tutorial/03_db_engine/db_engine_main.py
+++ official_tutorial/03_db_engine/db_engine_main.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from contextlib import closing
 
-test_db_engine = create_engine('sqlite:///:memory:', echo=True)
-
 if __name__ == '__main__':
+    test_db_engine = create_engine('sqlite:///:memory:', echo=True)
+
     with closing(test_db_engine.connect()) as conn:
         pass
